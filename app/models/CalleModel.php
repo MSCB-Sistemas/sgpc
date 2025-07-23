3fb96f6aba301ac6315e899b739c69d86bc532de
Ramiro Salcedo <ramirosdamian@gmail.com>
2025-07-23 12:01:09 -0300

    Avances sobre el modulo CalleModel
    Se agregaron metodos de insert, update, getcalle.

--- app/models/CalleModel.php
+++ app/models/CalleModel.php
@@ -13,8 +13,38 @@ class CalleModel
 
     public function getAllCalles(): array
     {
-        $stmt = $this->db->query("SELECT * FROM calles");
-        return $stmt->fetchAll();
+        $stmt = $this->db->prepare("SELECT * FROM calles");
+        // Ejecución de la consulta
+        $stmt->execute(); 
+        // Devuelve el resultado como un arreglo asociativo
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);  
     }
+
+    public function getCalle($id_calle): array 
+    {
+        $stmt = $this->db->prepare("SELECT * FROM calles WHERE id_calle = :id_calle");
+        $stmt->execute(['id_calle' => $id_calle]);
+        return $stmt->fetch();
+    }
+
+    public function updateCalle($id_calle, $nombre_calle): bool
+    {
+        $stmt = $this->db->prepare("UPDATE calles SET nombre = :nombre 
+        WHERE id_calle = :id_calle");
+        // Ejecuta la consulta pasando los valores
+        $stmt->execute(['id_calle' => $id_calle,'nombre' => $nombre_calle ]);
+        // Verifica si la actualización fue exitosa (si se afectaron filas)
+        return $stmt->rowCount() > 0;
+    }
+
+    public function insertCalle($nombre_calle)
+    {
+        $stmt = $this->db->prepare("INSERT INTO calles (nombre) VALUES (:nombre)");
+        // Ejecuta la consulta pasando los valores
+        $stmt->execute(['nombre' => $nombre_calle]);
+        return $this->db->lastInsertId();
+    }
+
 }
+
   
