d24ad69e47f810545de60df293cb263356037b37
OrellanaGalo <Galoorellana2@hotmail.com>
2025-07-25 11:28:44 -0300

    -- modificaciones layout pginicio.php
    --

--- app/views/pages/pginicio.php
+++ app/views/pages/pginicio.php
@@ -2,7 +2,7 @@
 <html lang="en" data-bs-theme="dark">
     <head>
         <meta charset="utf-8">
-        <title>Sidebar Example</title>
+        <title>Inicio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="<?= URL . '/css/bootstrap.min.css' ?>">
         <script src="<?= URL . '/js/bootstrap.bundle.min.js' ?>"></script>
@@ -106,7 +106,6 @@
             </ul>
         </div>
         <svg class="d-none" xmlns="http://www.w3.org/2000/svg">
-            <symbol id="bootstrap" viewBox="0 0 118 94"></symbol>
             <symbol id="home" viewBox="0 0 16 16"></symbol>
             <symbol id="speedometer2" viewBox="0 0 16 16"></symbol>
             <symbol id="table" viewBox="0 0 16 16"></symbol>
@@ -117,15 +116,7 @@
             <h1 class="visually-hidden">Barra inicio</h1>
             <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 289px;">
                 <a class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none" href="/">
-                    <svg class="bi pe-none me-2" width="40" height="32" aria-hidden="true">
-                        <use xlink:href="#bootstrap">
-                            <symbol id="bootstrap" viewBox="0 0 118 94">
-                                <title>Bootstrap</title>
-                                <path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"></path>
-                            </symbol>
-                        </use>
-                    </svg>
-                    <span class="fs-4">Sidebar</span>
+                    <img src="<?= URL . '/img/logo.png' ?>" alt="Logo" width="240" height="80" class="pe-none me-2">
                 </a>
                 <hr>
                 <ul class="nav nav-pills flex-column mb-auto">
@@ -138,7 +129,7 @@
                                     </symbol>
                                 </use>
                             </svg>
-                            Home
+                            Inicio
                         </a>
                     </li>
                     <li>
@@ -151,7 +142,7 @@
                                     </symbol>
                                 </use>
                             </svg>
-                            Dashboard
+                            Analiticas
                         </a>
                     </li>
                     <li>
@@ -163,7 +154,7 @@
                                     </symbol>
                                 </use>
                             </svg>
-                            A
+                            Registrar permiso
                         </a>
                     </li>
                     <li>
@@ -175,7 +166,7 @@
                                     </symbol>
                                 </use>
                             </svg>
-                            B
+                            Registrar chofer
                         </a>
                     </li>
                     <li>
