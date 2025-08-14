3426280ade8a6c0c4096daa66988f88855faf521
JonasPorro <jonas.porro@hotmail.com>
2025-08-14 10:55:54 -0300

    corregidos condicionales en header

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!-- Título dinámico desde PHP -->
-  <title><?= $datos['title'] ?? 'Sin título' ?></title>
+  <title><?php if(!empty($datos['title'])){echo $datos['title'];} else {echo 'Sin título';} ?></title>
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
